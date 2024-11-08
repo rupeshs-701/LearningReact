16b61acbed56c9de72e58886d8518992a9796269
theangryybeast <rupesh.ug21.cs@nitp.ac.in>
2024-11-08 22:16:53 +0530

    update Todo

--- reduxtoolkittodo/src/features/todo/todoSlice.js
+++ reduxtoolkittodo/src/features/todo/todoSlice.js
@@ -12,10 +12,25 @@ export const todoSlice = createSlice(
             addTodo:(state,action)=>{
                 const todo = {
                     id:nanoid(),
-                    text:action.payload
+                    text:action.payload 
+                }
+                state.todos.push(todo) 
+            },
+            removeTodo:(state,action)=>{
+                state.todos = state.todos.filter((todo)=> todo.id !== action.payload)
+            },
+            updateTodo : (state,action) => {
+                const {id, text} = action.payload
+                const todo =  state.todos.find((todo)=> todo.id === id)
+                if(todo){
+                    todo.text = text
                 }
             },
-            removeTodo:()=>{}
+            // deleteTodo : (state, action) => {
+            //     const {id} = action.payload
+            //     const todo = state.todos.filter((todo) => todo.id !== id)
+            //     state.todos = todo
+            // }
         }
     }
 )
