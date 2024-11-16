e9ad2beaab1dd46376b946bf962715c5a73b4f1d
rewanth <rupesh.ug21.cs@nitp.ac.in>
2024-11-17 02:27:38 +0530

    commit chnages

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
-      <h1>Blog App with appwrite</h1>
+      <h1>A Blog App with appwrite</h1>
     </>
   );
 }
---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,12 @@
+const conf = {
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId : String(import.meta.VITE_APPWRITE_PROJECT_ID),
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteDatabaseId : String(import.meta.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBuckettId : String(import.meta.VITE_APPWRITE_BUCKET_ID)
+
+
+}
+
+export default conf
